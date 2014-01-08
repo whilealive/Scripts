3a4e640230c0b505a43dbfb1236cbde484771438
whilealive <bruno.bischi@gmail.com>
2014-01-08 19:53:51 +0100

    initial commit

---
+++ musictogo.sh
@@ -0,0 +1,25 @@
+# ==================================================================
+# FILE     musictogo.sh
+# MACHINE  laptop (?) --> where the ogg library is...
+# INFO     randomly chooses albums from library and copies them to 
+#          mobile player
+#
+# DATE     08.01.2014
+# OWNER    Bischofberger
+# ==================================================================
+
+# TODO 
+# - 
+
+
+# variables
+library="$HOME/Music"
+player="/run/media/$(hostname)/..."
+limit=30*1024  # size of player = 30GB
+sum=0
+
+
+# Grösse des albums berechnen,
+# wenn summe mit albumsize addiert grösser als limit, nicht hinzufügen
+# mit Kopieren auf Player fortfahren
+albumsize=`du --block-size=MB -s $album/`
