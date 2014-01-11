b7dc863405baa96fbb09d01911297d0db4c89428
whilealive <bischofberger@fastmail.fm>
2014-01-11 16:05:05 +0100

    added TODO

--- zathuratab.sh
+++ zathuratab.sh
@@ -7,5 +7,13 @@
 # OWNER    Bischofberger
 # ==================================================================
 
+# TODO:
+# if(!exists tabbed.xid)
+#     starte tabbed mit zathura und $1
+# else
+#     redirect zathura mit $1
+#
+# Dann von ranger diesen Befehl aufrufen
+
 $(tabbed -c -d >/tmp/tabbed.xid)
 zathura -e $(</tmp/tabbed.xid)
