9f3ea69c1fca5804a325490f000f4b994aa4b0eb
whilealive <bruno.bischi@gmail.com>
2014-01-15 07:37:38 +0100

    start mutt automatically

--- mutt-session/mutt-session-tmux-init.sh
+++ mutt-session/mutt-session-tmux-init.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 #------------------------------------------
 # mutt-session-tmux-init
-# by Bruno Bischofberger, 04.01.2014
+# by Bruno Bischofberger, 15.01.2014
 #
 # initializes a new tmux sesseion with mutt
 # and mbsync-secure in seperate windows
 #------------------------------------------
 
-#tmux new-session -d -s mail 'mbsync-secure'
-#tmux new-window -n mutt -t mail:1
-#tmux select-window -t mail:0
-#tmux -2 attach-session -t mail
-
-tmux new-session -d -n mail -s mail 
+tmux new-session -d -n mail -s mail 'mutt'
 tmux new-window -n mbsync-secure -t mail:1 'mbsync-secure'
 tmux select-window -t mail:0
 tmux -2 attach-session -t mail
