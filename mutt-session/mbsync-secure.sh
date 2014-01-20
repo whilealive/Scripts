b95374b3e8bda24b508bf629a63ca5d2b34fbf7c
whilealive <bruno.bischi@gmail.com>
2014-01-20 11:31:55 +0100

    added new mail notification

--- mutt-session/mbsync-secure.sh
+++ mutt-session/mbsync-secure.sh
@@ -6,7 +6,7 @@
 #------------------------------------------
 
 # TODO:
-# use "sponge" from "moreutils"
+# use "sponge" from "moreutils" ?
 # title change
 # give a total of new messages at the bottom
 
@@ -14,6 +14,10 @@
 # variables
 period="10"
 let "period_sec = 60 * $period"
+
+INBOX1=$HOME/Maildir/fastmail/INBOX/new/
+INBOX2=$HOME/Maildir/uzh-pseudo/INBOX_uzh/new/
+INBOX3=$HOME/Maildir/zhaw-pseudo/INBOX_zhaw/new/
 
 passwd1="$(gpg2 -dq ~/.my-pwds.gpg | cut -c 40- | sed -n 1p)"
 passwd3="$(gpg2 -dq ~/.my-pwds.gpg | cut -c 40- | sed -n 3p)"
@@ -28,14 +32,22 @@ do
 	sed s/\#Pass3/Pass\ $passwd3/ ~/.mbsyncrc-temp1 > ~/.mbsyncrc-temp2
 	sed s/\#Pass2/Pass\ $passwd2/ ~/.mbsyncrc-temp2 > ~/.mbsyncrc-temp
 
-	# run mbsync using the temporary rc file
-	mbsync -c ~/.mbsyncrc-temp -a
+	# run mbsync using the temporary rc file and delete it aftwerwards
+	mbsync -q -c ~/.mbsyncrc-temp -a
+	shred -xu ~/.mbsyncrc-temp1 ~/.mbsyncrc-temp2 ~/.mbsyncrc-temp
+
+	# count new messages
+	NEW1=`find $INBOX1 -type f | wc -l`
+	NEW2=`find $INBOX2 -type f | wc -l`
+	NEW3=`find $INBOX3 -type f | wc -l`
+	echo -e "fastmail new:\t $NEW1"
+	echo -e "uzh new:\t $NEW1"
+	echo -e "zhaw new:\t $NEW1"
+
+	# some information
 	echo -n -e "\nlast sync: "
 	date --utc +"%H:%M"
 	echo "next sync in $period minutes."
-
-	# delete the insecure copy of the rc file again
-	shred -xu ~/.mbsyncrc-temp1 ~/.mbsyncrc-temp2 ~/.mbsyncrc-temp
 
 	# either wait for 15 minutes or check again immediately by pressing Enter
 	echo "to sync mails now, press [Enter]"
