176c555e26414f203253f92b7047ab93e5356ba7
whilealive <bruno.bischi@gmail.com>
2014-01-25 21:16:41 +0100

    functions()

--- musictogo.sh
+++ musictogo.sh
@@ -19,25 +19,26 @@ player="/run/media/$(hostname)/..."
 limit=30*1024  # size of player = 30GB
 sum=0
 
-
-# Grösse des albums berechnen,
-# wenn summe mit albumsize addiert grösser als limit, nicht hinzufügen
-# mit Kopieren auf Player fortfahren
-
-
-cd $library
-
-# list all albums in a temporary file
-find . -type d -links 2 > albumlist.tmp                     # search for directories which have no more subdirectories
-nl --number-separator=": " albumlist.tmp > albumlist2.tmp   # add line numbers
-awk '{$1=$1}1' albumlist2.tmp > albumlist.tmp               # delete all leading spaces
-rm albumlist2.tmp
+# list all albums in a temporary file called "albumlist.tmp"
+find_albums() {
+    echo -n "Collecting music library information..."
+    cd $library
+    find . -type d -links 2 > albumlist.tmp                     # search for directories which have no more subdirectories
+    nl --number-separator=": " albumlist.tmp > albumlist2.tmp   # add line numbers
+    awk '{$1=$1}1' albumlist2.tmp > albumlist.tmp               # delete all leading spaces
+    rm albumlist2.tmp
+    echo "done."
+}
 
 # randomly choose a new album from the list, and check its size
-max=`wc -l < albumlist.tmp`
-nextnumbr=`expr $RANDOM % $max`
-nextalbum=`awk -F:\  '$1 ~ /^'$nextnumbr'$/ {print $2}' albumlist.tmp`
-albumsize=`du --block-size=MB -s "$nextalbum" | cut -f1 | grep -o '[0-9]\+'`
-echo $albumsize
+rand_choose() {
+    max=`wc -l < albumlist.tmp`
+    nextnumbr=`expr $RANDOM % $max`
+    nextalbum=`awk -F:\  '$1 ~ /^'$nextnumbr'$/ {print $2}' albumlist.tmp`
+    albumsize=`du --block-size=MB -s "$nextalbum" | cut -f1 | grep -o '[0-9]\+'`
+}
 
 #rm albumlist.tmp
+find_albums
+rand_choose
+echo $albumsize
