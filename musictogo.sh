928db8a2ea9208e70d28a332b885ad4f294da6cd
whilealive <bruno.bischi@gmail.com>
2014-01-26 21:41:46 +0100

    added free-check

--- musictogo.sh
+++ musictogo.sh
@@ -4,20 +4,33 @@
 # MACHINE  laptop (?) --> where the ogg library is...
 # INFO     randomly chooses albums from library and copies them to 
 #          mobile player
+# DEPENDS  "udisk_functions" from the AUR
 #
-# DATE     25.01.2014
+# DATE     26.01.2014
 # OWNER    Bischofberger
 # ==================================================================
 
 # TODO 
-# - 
+# execute sdbm command from here to mount partition
 
 
 # variables
 library="$HOME/Music"
-player="/run/media/$(hostname)/..."
-limit=30*1024  # size of player = 30GB
+player="/run/media/$(hostname)/syncstick"  # after mounting with "udisk_functions"
 sum=0
+
+# check free space on player
+# new global variables: $limit
+check_free_space() {
+      # mount player with "udisk_functions"
+    if [ -e $player ] ; then
+        freespace=`df -h $usb_path | awk -F'[^0-9]*' 'NR==2 {print $6}'`
+        ((freespace -= 1))  # for safety
+    else
+        echo "Please plug in player/card and rerun programme."
+        exit
+    fi
+}
 
 # list all albums in a temporary file called "albumlist.tmp"
 find_albums() {
@@ -31,14 +44,18 @@ find_albums() {
 }
 
 # randomly choose a new album from the list, and check its size
+# new global variables: $nextalbum, $albumsize
 rand_choose() {
-    max=`wc -l < albumlist.tmp`
-    nextnumbr=`expr $RANDOM % $max`
+    local max=`wc -l < albumlist.tmp`
+    local nextnumbr=`expr $RANDOM % $max`
     nextalbum=`awk -F:\  '$1 ~ /^'$nextnumbr'$/ {print $2}' albumlist.tmp`
     albumsize=`du --block-size=MB -s "$nextalbum" | cut -f1 | grep -o '[0-9]\+'`
 }
 
 #rm albumlist.tmp
+check_free_space
+echo $freespace
 find_albums
 rand_choose
 echo $albumsize
+# unmount
